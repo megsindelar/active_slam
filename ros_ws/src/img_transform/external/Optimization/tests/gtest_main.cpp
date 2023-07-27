f343286baffab906016beb8c7dfd8c00284f27b7
Meg <megansindelar2023@u.northwestern.edu>
2023-07-27 14:11:06 -0500

    linked SESync package

--- ros_ws/src/img_transform/external/Optimization/tests/gtest_main.cpp
+++
@@ -1,9 +0,0 @@
-/// This short executable simply runs all registered GoogleTests
-
-#include <gtest/gtest.h>
-
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
--- ros_ws/src/img_transform/external/build/CMakeFiles/SESync.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SESync.
-# This may be replaced when dependencies are built.
