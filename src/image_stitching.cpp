fe77f945e5c719643d756faa5a3c2c11bbfec60f
Meg <megansindelar2023@u.northwestern.edu>
2023-08-30 14:33:16 -0500

    updating files

--- src/image_stitching.cpp
+++ src/image_stitching.cpp
@@ -6,12 +6,20 @@
 
 #include <iostream>
 
-int main() {
-    // cv::Mat image1 = cv::imread("image/image.jpg");
-    // cv::Mat image2 = cv::imread("image/image2.jpg");
+int main( int argc, char** argv ) {
+\
+    auto begin_total = std::chrono::high_resolution_clock::now();
+    if ( argc != 3 )
+    {
+        std::cout<<"usage: image_stitching before_file after_file"<<std::endl;
+        return 1;
+    }
 
     cv::FileStorage fs;
-    fs.open("../ros_ws/cv_images_full_square.ext", cv::FileStorage::READ);
+    fs.open(argv[2], cv::FileStorage::READ);
+
+    cv::FileStorage fs_before;
+    fs_before.open(argv[1], cv::FileStorage::READ);
 
     int num = (int) fs["num"];
 
@@ -21,83 +29,34 @@ int main() {
 
     double theta = 0.0;
     double rotation_angle = 0.0;
+    double delta_angle = 0.0;
     int img2_x = 0;
     int img2_y = 0;
     double curr_pos_x = 0.0;
     double curr_pos_y = 0.0;
+    int ind_loop = 0;
+    bool loop = false;
+
+    int loop_id_1 = num-1;
 
     for (int i = 0; i < num; i++){
 
-        double window_col = 0.07629; //0.08825;
-        double window_row = 0.07429; //0.08029;
+        double window_col = 0.07629;
+        double window_row = 0.07429;
 
         cv::Mat image2 = fs["image" + std::to_string(i+1)].mat();
 
-        double translation_x = (double) fs["transform_x" + std::to_string(i+1)];
-        double translation_y = (double) fs["transform_y" + std::to_string(i+1)];
-        rotation_angle += -(double) fs["transform_theta" + std::to_string(i)];
-        // double rotation_angle = 0;
+        double translation_x = (double) fs["transform_x" + std::to_string(i)];
+        double translation_y = (double) fs["transform_y" + std::to_string(i)];
 
-        std::cout << "rotation_angle: " << rotation_angle << std::endl;
+        theta = -(double) fs_before["transform_theta" + std::to_string(i)];
 
-        std::cout << "theta: " << theta << std::endl;
+        rotation_angle += theta;
 
-        // rotation_angle += theta;
-        // theta = rotation_angle;
-
-        // if (abs(rotation_angle) > 40){
-        //     rotation_angle += theta;
-        //     theta = rotation_angle;
-        //     std::cout << "Test 1000" << std::endl;
-        // }
-        // else{
-        //     std::cout << "Test 3000" << std::endl;
-        // }
-
-        // else if (abs(rotation_angle) > 40){
-        //     // rotation_angle += theta;
-        //     rotation_angle = -45;
-        //     theta = rotation_angle;
-        //     std::cout << "Test 2000" << std::endl;
-            // theta = 0.0;
-            // if (abs(translation_y) > abs(translation_x) && translation_y < 0.0){
-            //     translation_x += 3.0*window_row/5.0;
-            //     std::cout << "Test 100" << std::endl;
-            // }
-            // else if (abs(translation_y) > abs(translation_x) && translation_y > 0.0){
-            //     translation_x -= 3.0*window_row/5.0;
-            //     std::cout << "Test 200" << std::endl;
-            // }
-            // else if (abs(translation_y) < abs(translation_x) && translation_x < 0.0){
-            //     translation_y += 3.0*window_col/5.0;
-            //     std::cout << "Test 300" << std::endl;
-            // }
-            // else if (abs(translation_y) < abs(translation_x) && translation_x > 0.0){
-            //     translation_y -= 3.0*window_col/5.0;
-            //     std::cout << "Test 400" << std::endl;
-            // }
-            // else{
-            //     translation_x += 0;
-            //     translation_y += 0;
-            //     std::cout << "Test 500" << std::endl;
-            // }
-        // }
-        // else{
-        //     std::cout << "Test 3000" << std::endl;
-        //     // theta = 0.0;
-        // }
-
-
-        std::cout << "image2: row, col " << image2.rows << " " << image2.cols << std::endl;
-
-
-        std::cout << "translation x: " << translation_x << std::endl;
-        std::cout << "translation y: " << translation_y << std::endl;
-        std::cout << "rotation_angle: " << rotation_angle << std::endl;
-
-        // double rotation_angle = 0;
-        // double translation_x = -0.7;
-        // double translation_y = 0;
+        if (i == num-1){
+            ind_loop = 0;
+            loop = true;
+        }
 
 
         // Compute the rotation matrix
@@ -109,8 +68,8 @@ int main() {
         double ppm_row = image2.rows/window_row;
 
         // Apply translation to the rotation matrix
-        // rotation_matrix.at<double>(0, 2) += translation_x; //*ppm_row;
-        // rotation_matrix.at<double>(1, 2) += translation_y; //*ppm_col;
+        rotation_matrix.at<double>(0, 2) += translation_x; //*ppm_row;
+        rotation_matrix.at<double>(1, 2) += translation_y; //*ppm_col;
 
         cv::Size rotatedSize(image2.cols, image2.rows);
 
@@ -118,16 +77,6 @@ int main() {
         cv::warpAffine(image2, warped_image2, rotation_matrix, rotatedSize);
 
         // Calculate dimensions of the stitched image
-
-
-        // // stitching horizontally
-        // int stitched_width = image1.cols + image2.cols;
-        // int stitched_height = std::max(image1.rows, image2.rows);
-
-        // cv::Mat stitched_image(stitched_height, stitched_width, image1.type());
-        // image1.copyTo(stitched_image(cv::Rect(0, 0, image1.cols, image1.rows)));
-        // warped_image2.copyTo(stitched_image(cv::Rect(image1.cols, 0, warped_image2.cols, warped_image2.rows)));
-
 
         // stitching vertically
         int stitched_width = (image1.cols + abs(translation_x)*ppm_row);
@@ -171,19 +120,9 @@ int main() {
             t_y = curr_pos_y + translation_y*ppm_row;
             curr_pos_x = t_x;
             curr_pos_y += 0.0;
-            // curr_pos_x = 0.0;
-            // curr_pos_y = 0.0;
-        }
-        else if (translation_x < 0.0 && translation_y < 0.0){
-            std::cout << "Test 4" << std::endl;
-            stitch_x = (int)(abs(translation_x*ppm_row));
-            stitch_y = (int)(abs(translation_y*ppm_col));
-            t_x = translation_x*ppm_row;
-            t_y = translation_y*ppm_col;
-            curr_pos_x = 0.0;
-            curr_pos_y = 0.0;
         }
         else{
+            std::cout << "Test 4" << std::endl;
             stitch_x = (int)(abs(translation_x*ppm_row));
             stitch_y = (int)(abs(translation_y*ppm_col));
             t_x = curr_pos_x + translation_x*ppm_row;
